d05881ffd8991ba06720b2385adaefc52bacab88
Vishal Chaudhary <vishalchaudhary5794@gmail.com>
2023-12-26 23:57:00 +0530

    Implemented Throttling

--- Ecom_Backend/settings.py
+++ Ecom_Backend/settings.py
@@ -140,6 +140,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_THROTTLE_CLASSES': ['rest_framework.throttling.UserRateThrottle'],
+    'DEFAULT_THROTTLE_RATES': {'user': '15/hour'}
 }
 
 SIMPLE_JWT = {

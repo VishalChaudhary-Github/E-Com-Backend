235e9554ebdcb502e76c2b54c669f55c3a24938a
Vishal Chaudhary <vishalchaudhary5794@gmail.com>
2023-12-30 15:02:46 +0530

    Finalized Changes #2

--- Ecom_Backend/settings.py
+++ Ecom_Backend/settings.py
@@ -151,6 +151,6 @@ SIMPLE_JWT = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379"
+        "LOCATION": env('REDIS')
     }
 }
